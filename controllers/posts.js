90dede86095857a69fd0c2517878c726426a18b3
ante <ante.kotarac@gmail.com>
2022-11-04 13:30:38 +0100

    server renaming model

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import PostMessage from "../models/postMessage.js";
+import Post from "../models/post.js";
 
 export const getPost = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const post = await PostMessage.findById(id);
+    const post = await Post.findById(id);
 
     res.status(200).json(post);
   } catch (error) {
@@ -18,8 +18,8 @@ export const getPosts = async (req, res) => {
   try {
     const limit = 9;
     const startIndex = (Number(page) - 1) * limit;
-    const total = await PostMessage.countDocuments({});
-    const posts = await PostMessage.find().sort({ _id: -1 }).limit(limit).skip(startIndex);
+    const total = await Post.countDocuments({});
+    const posts = await Post.find().sort({ _id: -1 }).limit(limit).skip(startIndex);
     res.status(200).json({ posts, currentPage: Number(page), numberOfPages: Math.ceil(total / limit), });
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -30,7 +30,7 @@ export const getPostsSearch = async (req, res) => {
   try {
     const { searchQuery } = req.query;
     const title = new RegExp(searchQuery, 'i');
-    const posts = await PostMessage.find({ $or: [{ title }] });
+    const posts = await Post.find({ $or: [{ title }] });
     res.status(200).json({ posts });
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -40,7 +40,7 @@ export const getPostsSearch = async (req, res) => {
 export const createPosts = async (req, res) => {
   try {
     const post = req.body;
-    const response = await PostMessage.create({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+    const response = await Post.create({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     res.status(201).json(response);
   } catch (error) {
     res.status(409).json(error);
@@ -51,12 +51,12 @@ export const updatePosts = async (req, res) => {
   try {
     const { id } = req.params
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).send('Post with that id not found!')
+      return res.status(404).send('Image with that id not found!')
     }
 
     const { body } = req;
 
-    const updatePost = await PostMessage.findByIdAndUpdate(id, body, { new: true });
+    const updatePost = await Post.findByIdAndUpdate(id, body, { new: true });
     res.json(updatePost)
 
   } catch (error) {
@@ -69,12 +69,12 @@ export const deletePosts = async (req, res) => {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).send('Post with that id not found!')
+      return res.status(404).send('Image with that id not found!')
     }
 
-    const response = await PostMessage.deleteOne({ _id: id })
+    const response = await Post.deleteOne({ _id: id })
     const { deletedCount } = response;
-    const message = deletedCount > 0 ? 'Post deleted succesfully' : 'Post not deleted'
+    const message = deletedCount > 0 ? 'Image deleted succesfully' : 'Image not deleted'
     res.status(200).json({ message })
   } catch (error) {
     res.status(404).json({ message: error.message })
