8abcda6202c2f54a438378c163b81e61255fac19
ante <ante.kotarac@gmail.com>
2022-11-05 21:23:03 +0100

    remove pub static files

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(express.static("public"));
+app.use('/', express.static("public"));
+app.use('/images', express.static("images"));
 app.use("/posts", postsRouter);
 app.use("/user", userRouter);
 
